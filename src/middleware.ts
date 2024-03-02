00ec89ffca276a8d34af41cd43453aae8d2dc68d
AnshV2 <misteransh@gmail.com>
2024-03-02 07:48:52 -0500

    fixed permisison stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
-
-//publicRoutes: ['/anyone-can-visit-this-route', '/', '/signin'],
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ['/anyone-can-visit-this-route', '/', '/signin', '/profile'],
   // Routes that can always be accessed, and have
   // no authentication information
- // ignoredRoutes: ['/no-auth-in-this-route'],
+  ignoredRoutes: ['/no-auth-in-this-route'],
+});
+
  
 export const config = {
   // Protects all routes, including api/trpc.
